e5728a944b140bd71af21b52a43005cdeca65126
Gowtamkumar <gowtampaul0@gmail.com>
2022-05-12 23:53:13 +0600

    basic redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { actions } from './store/store'
+
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch(actions.increment());
+  }
+
+  const decrement = () => {
+    dispatch(actions.decrement());
+  }
+
+  const addBy = () => {
+    dispatch(actions.addBy(12));
+  }
   return (
     <div>
-      <h2>Reduxt toolkit</h2>
+      <h2>{counter}</h2>
+      <button onClick={increment} >Increment</button>
+      <button onClick={decrement} >Increment</button>
+      <button onClick={addBy} >Increment</button>
+
     </div>
   );
 }
